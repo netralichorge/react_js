98b996ac485a5edbde23c478eb2f59e48498b712
netralichorge <netrali.chorge20@gmail.com>
2025-01-10 08:46:49 +0530

    fetch Git Hub data

--- second_app/src/components/GitHubCard.jsx
+++ second_app/src/components/GitHubCard.jsx
@@ -2,29 +2,68 @@ import React, { useEffect, useState } from 'react'
 
 function GitHubCard() {
 
-    let [gitHubData,setGitHubData] = useState({});
-    useEffect(()=>{
+    let [gitHubData, setGitHubData] = useState({}); // It return an object
+    let [repositories, setRepositories] = useState([]) // It return an array 
+    useEffect(() => {
 
         fetch("https://api.github.com/users/netralichorge") // github link
-        .then(data=>data.json())
-        .then(data=>{
-           // console.log(data["followers"]);
-           console.log(gitHubData)
-           setGitHubData(data);
-           
-            
-        })
+            .then(data => data.json())
+            .then(data => {
+                // console.log(data["followers"]);
 
-    },[])
+                setGitHubData(data);
+                // console.log(gitHubData)
+
+
+                // Repositories
+                fetch(data.repos_url)
+                    .then(repos => repos.json())
+                    .then(repos => {
+
+                        // console.log("Repos"+repos);
+                        setRepositories(repos);
+                    })
+
+            })
+
+    }, [])
     return (
         <div>
+            {/*Profile Start */}
             <h1>GitHub Profile</h1>
             <div className="card github-card" >
-                <img src="..." className="card-img-top" alt="..." />
+                <img src="{gitHubData.avatar_url}" className="card-img-top" alt="..." />
                 <div className="card-body">
-                    <p className="card-text"> </p>
+                    <p className="card-text">Name :{gitHubData.name} </p>
+                    <p className="card-text">Followers :{gitHubData.followers} </p>
+                    <a href={gitHubData.html_url}>Click here to visit github profile </a>
+
                 </div>
             </div>
+            {/* Profile end */}
+
+            {/*Repositories : Start */}
+
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Repository Name</th>
+                        <th scope="col">Repository link</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    {repositories.map((repoObj)=>{
+                        return(<tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                        </tr>)
+                    })}
+                
+                </tbody>
+            </table>
+
+            {/*Repositories : End */}
         </div>
     )
 }
